8529887ceac7ccd15aa9f630795317f288fdd75f
이영한 <mashiro@snu.ac.kr>
2021-07-11 14:03:55 +0900

    bookmark tab list edit

--- app/src/main/java/com/lee/book/ui/bookmark/BookmarkFragment.kt
+++ app/src/main/java/com/lee/book/ui/bookmark/BookmarkFragment.kt
@@ -1,20 +1,41 @@
 package com.lee.book.ui.bookmark
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.lee.book.databinding.FragmentBookmarkBinding
 import com.lee.book.entitiy.Book
+import java.util.*
+import kotlin.collections.ArrayList
 
 class BookmarkFragment : Fragment() {
     private var _fragmentBookmarkBinding: FragmentBookmarkBinding? = null
     private val fragmentBookmarkBinding get() = _fragmentBookmarkBinding!!
-    private val bookmarkViewModel : BookmarkViewModel by activityViewModels()
+    private val bookmarkViewModel: BookmarkViewModel by activityViewModels()
     private var bookmarkList = ArrayList<Book>()
+    private val itemTouchHelper = ItemTouchHelper(object : ItemTouchHelper.SimpleCallback(ItemTouchHelper.UP or ItemTouchHelper.DOWN or ItemTouchHelper.START or ItemTouchHelper.END, 0) {
+        override fun onMove(recyclerView: RecyclerView, viewHolder: RecyclerView.ViewHolder, target: RecyclerView.ViewHolder): Boolean {
+            val fromPosition = viewHolder.adapterPosition
+            val toPosition = target.adapterPosition
+
+            Collections.swap(bookmarkViewModel.bookMarkedBooks.value!!, fromPosition, toPosition)
+
+            recyclerView.adapter?.notifyItemMoved(fromPosition, toPosition)
+
+            return true
+        }
+
+        override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+            Log.d("ItemTouchHelperTest", "onSwiped")
+        }
+    })
 
     override fun onCreateView(
             inflater: LayoutInflater,
@@ -31,6 +52,8 @@ class BookmarkFragment : Fragment() {
             bookmarkList.addAll(it)
             adapter.notifyDataSetChanged()
         })
+
+        itemTouchHelper.attachToRecyclerView(fragmentBookmarkBinding.bookmarkRecyclerView)
 
         return fragmentBookmarkBinding.root
     }
